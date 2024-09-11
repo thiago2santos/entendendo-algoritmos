319255ada235de036e5ee3e139c8c44278853bb7
thiago2santos <thisanti@hotmail.com>
2024-09-11 07:48:32 -0300

    QuickSort implementado em Go

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -12,3 +12,23 @@ func OrdenarPorSelecao[T Number](array []T) (arrayOrdenado []T) {
 	}
 	return
 }
+
+func OrdenarRapido[T Number](array []T) (arrayConcatenado []T) {
+	if len(array) < 2 {
+		return array
+	} else {
+		iPivo := len(array) / 2
+		vPivo := array[iPivo]
+		menores := make([]T, 0)
+		maiores := make([]T, 0)
+		for _, e := range array {
+			if e < vPivo {
+				menores = append(menores, e)
+			} else if e > vPivo {
+				maiores = append(maiores, e)
+			}
+		}
+		return slices.Concat(append(OrdenarRapido(menores), vPivo), OrdenarRapido(maiores))
+	}
+
+}
